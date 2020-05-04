0889faffe77335aa17dd66e869de00d79b0b8efa
Ubuntu <ubuntu@ip-172-31-15-208.eu-west-1.compute.internal>
2020-05-04 16:38:30 +0000

    cluster enabled

--- terraform/EC2/jenkins_setup.sh
+++ terraform/EC2/jenkins_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt update && sudo apt upgrade -y
 sudo apt install openjdk-8-jdk -y
-#Jenkins:
+#JENKINS:
 wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb https://pkg.jenkins.io/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update
@@ -9,5 +9,14 @@ sudo apt-get install jenkins -y
 #AWS-CLI
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 sudo apt install unzip -y
+unzip awscliv2.zip
 sudo ./aws/install 
-sleep 30
+#KUBECTL
+curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.15.10/2020-02-22/bin/linux/amd64/kubectl
+curl -o kubectl.sha256 https://amazon-eks.s3.us-west-2.amazonaws.com/1.15.10/2020-02-22/bin/linux/amd64/kubectl.sha256
+openssl sha1 -sha256 kubectl
+chmod +x ./kubectl
+mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
+echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
+kubectl version --short --client
+
