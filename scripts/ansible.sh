bf7dd8f84ee3cfa5dae3d90314e2a1fd94803d0b
Bailey-Elson <baileymitchellelson@gmail.com>
2020-05-05 15:07:17 +0100

    Full Jenkins

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get install python python3-pip -y 
+sudo apt-get install python3 python3-pip -y 
 # make sure ~/.local/bin exists and is on your PATH
 mkdir -p ~/.local/bin
 echo 'PATH=$PATH:~/.local/bin' >> ~/.bashrc
 source ~/.bashrc
 ## install ansible with pip
-pip install --user ansible
+pip3 install --user ansible
 # check that ansible has been installed
 ansible --version
 
